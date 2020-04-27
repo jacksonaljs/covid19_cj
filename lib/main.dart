d19ef887ff4ed1214aafb7c43ab8d6eb0e31bae7
Jackson Chelote <jacksonaljs@gmail.com>
2020-04-27 07:06:50 +0530

    Orientation Lock

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:covid19cj/homepage.dart';
 import 'package:flutter/material.dart';
 
 void main(){
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     theme: ThemeData(
