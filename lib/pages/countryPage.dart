12d7277bee08fe35fecbfbe06146944a6523bb7f
Jackson Chelote <jacksonaljs@gmail.com>
2020-04-27 19:01:40 +0530

    Added search in all countries list

--- lib/pages/countryPage.dart
+++ lib/pages/countryPage.dart
@@ -1,3 +1,4 @@
+import 'package:covid19cj/pages/search.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
@@ -28,40 +29,52 @@ class _CountryPageState extends State<CountryPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.search),onPressed: (){
+            showSearch(context: context, delegate: Search(countriesData));
+          },)
+        ],
         title: Text('Country Wise Stats'),
       ),
       body: countriesData==null?Center(child: CircularProgressIndicator(),):ListView.builder(
         itemBuilder: (context, index) {
-          return Container(
-            height: 130,
-            margin: EdgeInsets.symmetric(horizontal: 10,vertical: 10 ),
-            decoration: BoxDecoration(color: Colors.white,boxShadow: [
-              BoxShadow(color: Colors.grey[100],blurRadius: 10,offset: Offset(0,10)),
-            ]),
-            child: Row(
-               children: <Widget>[
-                 Container(
-                   margin: EdgeInsets.symmetric(horizontal: 10),
-                   child: Column (
-                     mainAxisAlignment: MainAxisAlignment.center,
-                     children: <Widget>[
-                       Text(countriesData[index]['country'],style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
-                       Image.network(countriesData[index]['countryInfo']['flag'],height: 80,width: 90,),
-                     ],
+          return Card(
+            margin: EdgeInsets.symmetric(vertical: 15),
+            child: Container(
+              //color: Colors.black12 ,
+              height: 130,
+              margin: EdgeInsets.symmetric(horizontal: 10,vertical: 10 ),
+//            decoration: BoxDecoration(color: Colors.white,boxShadow: [
+//              BoxShadow(color: Colors.grey[100],blurRadius: 10,offset: Offset(0,10)),
+//            ]),
+              child: Row(
+                 children: <Widget>[
+                   Container(
+                     width: 180,
+                     margin: EdgeInsets.symmetric(horizontal: 10),
+                     child: Column (
+                       crossAxisAlignment: CrossAxisAlignment.start,
+                       mainAxisAlignment: MainAxisAlignment.center,
+                       children: <Widget>[
+                         Text(countriesData[index]['country'],style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
+                         Image.network(countriesData[index]['countryInfo']['flag'],height: 90,width: 80,),
+                       ],
+                     ),
                    ),
-                 ),
-                 Expanded(child: Container(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text('CONFIRMED:' + countriesData[index]['cases'].toString(),style: TextStyle(fontWeight: FontWeight.bold,color: Colors.red,fontSize: 15),),
-                        Text('ACTIVE:' + countriesData[index]['active'].toString(),style: TextStyle(fontWeight: FontWeight.bold,color: Colors.blue,fontSize: 15),),
-                        Text('RECOVERED:' + countriesData[index]['recovered'].toString(),style: TextStyle(fontWeight: FontWeight.bold,color: Colors.green,fontSize: 15),),
-                        Text('DEATHS:' + countriesData[index]['deaths'].toString(),style: TextStyle(fontWeight: FontWeight.bold,color: Colors.grey[800],fontSize: 15),),
-                      ],
-                    ),
-                 ))
-               ],
+                   Expanded(child: Container(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text('CONFIRMED:' + countriesData[index]['cases'].toString(),style: TextStyle(fontWeight: FontWeight.bold,color: Colors.red,fontSize: 20),),
+                          Text('ACTIVE:' + countriesData[index]['active'].toString(),style: TextStyle(fontWeight: FontWeight.bold,color: Colors.blue,fontSize: 20),),
+                          Text('RECOVERED:' + countriesData[index]['recovered'].toString(),style: TextStyle(fontWeight: FontWeight.bold,color: Colors.green,fontSize: 20),),
+                          Text('DEATHS:' + countriesData[index]['deaths'].toString(),style: TextStyle(fontWeight: FontWeight.bold,color: Colors.grey[500],fontSize: 20),),
+                        ],
+                      ),
+                   ))
+                 ],
+              ),
             ),
           );
         },
