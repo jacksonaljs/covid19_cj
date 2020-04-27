d294df8511a2520cb16ab8abf9fac80f12bb13d8
Jackson Chelote <jacksonaljs@gmail.com>
2020-04-27 06:28:49 +0530

    FAQ UI

--- lib/datasource.dart
+++ lib/datasource.dart
@@ -11,22 +11,22 @@ class DataSource{
     {
       "question": "What is a coronavirus?",
       "answer":
-      "Coronaviruses are a large family of viruses which may cause illness in animals or humans.  In humans, several coronaviruses are known to cause respiratory infections ranging from the common cold to more severe diseases such as Middle East Respiratory Syndrome (MERS) and Severe Acute Respiratory Syndrome (SARS). The most recently discovered coronavirus causes coronavirus disease COVID-19."
+      "Coronaviruses are a large family of viruses which may cause illness in animals or humans.\n\nIn humans, several coronaviruses are known to cause respiratory infections ranging from the common cold to more severe diseases such as Middle East Respiratory Syndrome (MERS) and Severe Acute Respiratory Syndrome (SARS). \n\nThe most recently discovered coronavirus causes coronavirus disease COVID-19."
     },
     {
       "question": "What is COVID-19?",
       "answer":
-      "COVID-19 is the infectious disease caused by the most recently discovered coronavirus. This new virus and disease were unknown before the outbreak began in Wuhan, China, in December 2019."
+      "COVID-19 is the infectious disease caused by the most recently discovered coronavirus. \n\nThis new virus and disease were unknown before the outbreak began in Wuhan, China, in December 2019."
     },
     {
       "question": "What are the symptoms of COVID-19?",
       "answer":
-      "The most common symptoms of COVID-19 are fever, tiredness, and dry cough. Some patients may have aches and pains, nasal congestion, runny nose, sore throat or diarrhea. These symptoms are usually mild and begin gradually. Some people become infected but don’t develop any symptoms and don't feel unwell. Most people (about 80%) recover from the disease without needing special treatment. Around 1 out of every 6 people who gets COVID-19 becomes seriously ill and develops difficulty breathing. Older people, and those with underlying medical problems like high blood pressure, heart problems or diabetes, are more likely to develop serious illness. People with fever, cough and difficulty breathing should seek medical attention."
+      "The most common symptoms of COVID-19 are fever, tiredness, and dry cough. \n\nSome patients may have aches and pains, nasal congestion, runny nose, sore throat or diarrhea. \n\nThese symptoms are usually mild and begin gradually. \nSome people become infected but don’t develop any symptoms and don't feel unwell. \n\nMost people (about 80%) recover from the disease without needing special treatment. \n\nAround 1 out of every 6 people who gets COVID-19 becomes seriously ill and develops difficulty breathing. \n\nOlder people, and those with underlying medical problems like high blood pressure, heart problems or diabetes, are more likely to develop serious illness. \n\nPeople with fever, cough and difficulty breathing should seek medical attention."
     },
     {
       "question": "How does COVID-19 spread?",
       "answer":
-      "People can catch COVID-19 from others who have the virus. The disease can spread from person to person through small droplets from the nose or mouth which are spread when a person with COVID-19 coughs or exhales. These droplets land on objects and surfaces around the person. Other people then catch COVID-19 by touching these objects or surfaces, then touching their eyes, nose or mouth. People can also catch COVID-19 if they breathe in droplets from a person with COVID-19 who coughs out or exhales droplets. This is why it is important to stay more than 1 meter (3 feet) away from a person who is sick. \nWHO is assessing ongoing research on the ways COVID-19 is spread and will continue to share updated findings."
+      "People can catch COVID-19 from others who have the virus. \n\nThe disease can spread from person to person through small droplets from the nose or mouth which are spread when a person with COVID-19 coughs or exhales. \n\nThese droplets land on objects and surfaces around the person. \n\nOther people then catch COVID-19 by touching these objects or surfaces, then touching their eyes, nose or mouth. \n\nPeople can also catch COVID-19 if they breathe in droplets from a person with COVID-19 who coughs out or exhales droplets. \n\nThis is why it is important to stay more than 1 meter (3 feet) away from a person who is sick. \n\nWHO is assessing ongoing research on the ways COVID-19 is spread and will continue to share updated findings."
     },
     {
       "question":
@@ -37,18 +37,18 @@ class DataSource{
     {
       "question": "Can CoVID-19 be caught from a person who has no symptoms?",
       "answer":
-      "The main way the disease spreads is through respiratory droplets expelled by someone who is coughing. The risk of catching COVID-19 from someone with no symptoms at all is very low. However, many people with COVID-19 experience only mild symptoms. This is particularly true at the early stages of the disease. It is therefore possible to catch COVID-19 from someone who has, for example, just a mild cough and does not feel ill.  WHO is assessing ongoing research on the period of transmission of COVID-19 and will continue to share updated findings.    "
+      "The main way the disease spreads is through respiratory droplets expelled by someone who is coughing. \n\nThe risk of catching COVID-19 from someone with no symptoms at all is very low. However, many people with COVID-19 experience only mild symptoms. \n\nThis is particularly true at the early stages of the disease. It is therefore possible to catch COVID-19 from someone who has, for example, just a mild cough and does not feel ill.  \n\nWHO is assessing ongoing research on the period of transmission of COVID-19 and will continue to share updated findings.    "
     },
     {
       "question":
       "Can I catch COVID-19 from the feces of someone with the disease?",
       "answer":
-      "The risk of catching COVID-19 from the feces of an infected person appears to be low. While initial investigations suggest the virus may be present in feces in some cases, spread through this route is not a main feature of the outbreak. WHO is assessing ongoing research on the ways COVID-19 is spread and will continue to share new findings. Because this is a risk, however, it is another reason to clean hands regularly, after using the bathroom and before eating."
+      "The risk of catching COVID-19 from the feces of an infected person appears to be low. \n\nWhile initial investigations suggest the virus may be present in feces in some cases, spread through this route is not a main feature of the outbreak. \n\nWHO is assessing ongoing research on the ways COVID-19 is spread and will continue to share new findings. \n\nBecause this is a risk, however, it is another reason to clean hands regularly, after using the bathroom and before eating."
     },
     {
       "question": "How likely am I to catch COVID-19?",
       "answer":
-      "The risk depends on where you  are - and more specifically, whether there is a COVID-19 outbreak unfolding there.\nFor most people in most locations the risk of catching COVID-19 is still low. However, there are now places around the world (cities or areas) where the disease is spreading. For people living in, or visiting, these areas the risk of catching COVID-19 is higher. Governments and health authorities are taking vigorous action every time a new case of COVID-19 is identified. Be sure to comply with any local restrictions on travel, movement or large gatherings. Cooperating with disease control efforts will reduce your risk of catching or spreading COVID-19.\nCOVID-19 outbreaks can be contained and transmission stopped, as has been shown in China and some other countries. Unfortunately, new outbreaks can emerge rapidly. It’s important to be aware of the situation where you are or intend to go. WHO publishes daily updates on the COVID-19 situation worldwide."
+      "The risk depends on where you  are - and more specifically, whether there is a COVID-19 outbreak unfolding there.\n\nFor most people in most locations the risk of catching COVID-19 is still low. \n\nHowever, there are now places around the world (cities or areas) where the disease is spreading. \n\nFor people living in, or visiting, these areas the risk of catching COVID-19 is higher. \n\nGovernments and health authorities are taking vigorous action every time a new case of COVID-19 is identified. \n\nBe sure to comply with any local restrictions on travel, movement or large gatherings. \n\nCooperating with disease control efforts will reduce your risk of catching or spreading COVID-19.\n\nCOVID-19 outbreaks can be contained and transmission stopped, as has been shown in China and some other countries. \n\nUnfortunately, new outbreaks can emerge rapidly. It’s important to be aware of the situation where you are or intend to go. \n\nWHO publishes daily updates on the COVID-19 situation worldwide."
     },
     {
       "question": "Who is at risk of developing severe illness?",
@@ -58,13 +58,13 @@ class DataSource{
     {
       "question": "Should I wear a mask to protect myself?",
       "answer":
-      "Only wear a mask if you are ill with COVID-19 symptoms (especially coughing) or looking after someone who may have COVID-19. Disposable face mask can only be used once. If you are not ill or looking after someone who is ill then you are wasting a mask. There is a world-wide shortage of masks, so WHO urges people to use masks wisely.\nWHO advises rational use of medical masks to avoid unnecessary wastage of precious resources and mis-use of masks\nThe most effective ways to protect yourself and others against COVID-19 are to frequently clean your hands, cover your cough with the bend of elbow or tissue and maintain a distance of at least 1 meter (3 feet) from people who are coughing or sneezing"
+      "Only wear a mask if you are ill with COVID-19 symptoms (especially coughing) or looking after someone who may have COVID-19. \n\nDisposable face mask can only be used once. If you are not ill or looking after someone who is ill then you are wasting a mask. \n\nThere is a world-wide shortage of masks, so WHO urges people to use masks wisely.\n\nWHO advises rational use of medical masks to avoid unnecessary wastage of precious resources and mis-use of masks\n\nThe most effective ways to protect yourself and others against COVID-19 are to frequently clean your hands, cover your cough with the bend of elbow or tissue and maintain a distance of at least 1 meter (3 feet) from people who are coughing or sneezing"
     },
     {
       "question":
       "Are antibiotics effective in preventing or treating the COVID-19?",
       "answer":
-      "No. Antibiotics do not work against viruses, they only work on bacterial infections. COVID-19 is caused by a virus, so antibiotics do not work. Antibiotics should not be used as a means of prevention or treatment of COVID-19. They should only be used as directed by a physician to treat a bacterial infection. "
+      "No. Antibiotics do not work against viruses, they only work on bacterial infections. COVID-19 is caused by a virus, so antibiotics do not work. \n\nAntibiotics should not be used as a means of prevention or treatment of COVID-19. \n\nThey should only be used as directed by a physician to treat a bacterial infection. "
     }
   ];
 }
--- lib/pages/faqs.dart
+++ lib/pages/faqs.dart
@@ -6,9 +6,14 @@ class FAQPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('FAQs'),),
-      body:  ListView.builder(itemBuilder: (context,index){
-        return ExpansionTile(title: Text(DataSource.questionAnswers[index]['question']),children: <Widget>[
-          Text(DataSource.questionAnswers[index]['answer'])
+      body:  ListView.builder(
+          itemCount: DataSource.questionAnswers.length,
+          itemBuilder: (context,index){
+        return ExpansionTile(title: Text(DataSource.questionAnswers[index]['question'],style: TextStyle(fontWeight: FontWeight.bold,fontSize: 21),),children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Text(DataSource.questionAnswers[index]['answer'],style: TextStyle(fontSize: 14),),
+          )
         ],);
       }),
     );
